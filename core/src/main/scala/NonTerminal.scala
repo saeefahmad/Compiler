891246d5f850915510c3c561b301f2892a1c3c72
saeefbd <saeef.ahmad.bd@gmail.com>
2015-02-19 23:29:30 -0600

    updated code in Nonterminal.scala : revomed unused function

--- core/src/main/scala/NonTerminal.scala
+++ core/src/main/scala/NonTerminal.scala
@@ -44,28 +44,6 @@ object NonTerminal {
     case default => throw SyntaxError("Parse Error !!!")  
   } 
   
-  def deriveTerminal (str: String, inputStr: String): Array[String] = str match {    
-    case `program` => ParsingTable.program(inputStr)
-    case `declarations` => ParsingTable.declarations(inputStr)
-    case `identType` => ParsingTable.identType(inputStr)
-    case `statementSequence` => ParsingTable.statementSequence(inputStr)
-    case `statement` => ParsingTable.statement(inputStr)
-    case `assignment` => ParsingTable.assignment(inputStr)
-    case `assignmentPrime` => ParsingTable.assignmentPrime(inputStr)
-    case `ifStatement` => ParsingTable.ifStatement(inputStr)
-    case `elseClause` => ParsingTable.elseClause(inputStr)
-    case `whileStatement` => ParsingTable.whileStatement(inputStr)
-    case `writeInt` => ParsingTable.writeInt(inputStr)
-    case `expression` => ParsingTable.expression(inputStr)
-    case `expressionPrime` => ParsingTable.expressionPrime(inputStr)
-    case `simpleExpression` => ParsingTable.simpleExpression(inputStr)
-    case `simpleExpressionPrime` => ParsingTable.simpleExpressionPrime(inputStr)
-    case `term` => ParsingTable.term(inputStr)
-    case `termPrime` => ParsingTable.termPrime(inputStr)
-    case `factor` => ParsingTable.factor(inputStr)
-    case default => throw SyntaxError("Parse Error !!!")  
-  } 
-  
   def startSymbol (): String = {
     return program
   }
